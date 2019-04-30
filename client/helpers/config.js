cb97653bc56aa1e64152dc73241ea8a14edd9f4f
Enodi <enodiaudu5@gmail.com>
2019-04-30 16:25:36 +0100

    update environment variables

--- client/helpers/config.js
+++ client/helpers/config.js
@@ -3,7 +3,6 @@ import axios from "axios";
 const path = process.env.NODE_ENV === "production" ? 
   "https://tel-phone-number-generator.herokuapp.com": 
   "http://localhost:3201";
-
 const generatePhoneNumbers = (totalNumbersToBeGenerated, sortBy) => {
   return axios.post(`${path}/api/v1/phone-number?order=${sortBy}`, {
     totalNumbersToBeGenerated
